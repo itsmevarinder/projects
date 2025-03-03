dad374b407146cc36e6b7d20807e529d5ac94962
Varinder <115914598+itsmevarinder@users.noreply.github.com>
2025-03-03 08:22:36 +0530

    Add files via upload

---
+++ collapsible-gallery-css-only/README.md
@@ -0,0 +1,4 @@
+#  Collapsible Gallery | CSS Only
+
+A Pen created on CodePen.io. Original URL: [https://codepen.io/icomgroup/pen/BaXBdWw](https://codepen.io/icomgroup/pen/BaXBdWw).
+
---
+++ travel-carousel-design/script.js
@@ -0,0 +1,37 @@
+var swiper = new Swiper(".swiper", {
+  effect: "coverflow",
+  grabCursor: true,
+  centeredSlides: true,
+  slidesPerView: "auto",
+  coverflowEffect: {
+    rotate: 0,
+    stretch: 0,
+    depth: 100,
+    modifier: 2,
+    slideShadows: true
+  },
+  keyboard: {
+    enabled: true
+  },
+  mousewheel: {
+    thresholdDelta: 70
+  },
+  spaceBetween: 60,
+  loop: true,
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true
+  }
+});
+
+document.querySelector(".swiper").addEventListener("mouseenter", function () {
+  swiper.autoplay.stop();
+});
+
+document.querySelector(".swiper").addEventListener("mouseleave", function () {
+  swiper.autoplay.start();
+});
